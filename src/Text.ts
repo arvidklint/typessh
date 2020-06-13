f5aeba88535ad66fe434fc483caf3405fe004f1f
Arvid Sätterkvist <arvidsat@gmail.com>
2020-06-13 19:29:48 +0200

    feat: add scoreboard and restart

--- src/Text.ts
+++ src/Text.ts
@@ -5,13 +5,15 @@ const TEXT =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
 const MAX_ERRORS = 5;
-const HORIZONTAL_PADDING = 3;
-const TEXT_PADDING = 2;
+const HORIZONTAL_MARGIN = 3;
+const TEXT_MARGIN = 2;
 
 export default class Text {
     public box: blessed.Widgets.BoxElement;
     private header: blessed.Widgets.BoxElement;
     private body: blessed.Widgets.BoxElement;
+    private scoreBox: blessed.Widgets.BoxElement;
+    private score: blessed.Widgets.BoxElement;
     private stringLines: string[] = [];
     private lines: blessed.Widgets.BoxElement[] = [];
     private currentLine: number = 0;
@@ -21,7 +23,7 @@ export default class Text {
 
     constructor(public readonly id: string, private screen: Screen) {
         // Manually divide text into lines to fix line break bugs
-        const maxLength = this.width - TEXT_PADDING * 2 - MAX_ERRORS;
+        const maxLength = this.width - TEXT_MARGIN * 2 - MAX_ERRORS;
 
         this.stringLines = TEXT.split(' ').reduce(
             (acc: Array<string>, word: string) => {
@@ -45,8 +47,8 @@ export default class Text {
         this.box = blessed.box({
             screen: this.screen.screen,
             top: 5,
-            left: HORIZONTAL_PADDING,
-            right: HORIZONTAL_PADDING,
+            left: HORIZONTAL_MARGIN,
+            right: HORIZONTAL_MARGIN,
         });
 
         this.header = blessed.box({
@@ -60,8 +62,8 @@ export default class Text {
         this.body = blessed.box({
             parent: this.box,
             top: 2,
-            left: TEXT_PADDING,
-            right: TEXT_PADDING,
+            left: TEXT_MARGIN,
+            right: TEXT_MARGIN,
             height: this.stringLines.length,
         });
 
@@ -78,6 +80,37 @@ export default class Text {
             });
         });
 
+        this.scoreBox = blessed.box({
+            parent: this.box,
+            top: 2,
+            left: 'center',
+            width: 25,
+            height: 6,
+            align: 'center',
+            valign: 'middle',
+            border: 'line',
+        });
+
+        this.score = blessed.box({
+            parent: this.scoreBox,
+            top: 1,
+            left: 0,
+            right: 0,
+            align: 'center',
+            type: 'line',
+            content: 'WPM: __',
+        });
+
+        blessed.box({
+            parent: this.scoreBox,
+            top: 3,
+            left: 0,
+            right: 0,
+            align: 'center',
+            content: 'Ctrl+R to restart',
+            type: 'line',
+        });
+
         this.listen();
     }
 
@@ -85,12 +118,25 @@ export default class Text {
         return this.startTime !== 0;
     }
 
-    private get width(): number {
-        return this.screen.info.cols - HORIZONTAL_PADDING * 2;
+    private get completed(): boolean {
+        return (
+            this.started &&
+            this.currentLine === this.stringLines.length - 1 &&
+            this.currentCol === this.stringLines[this.currentLine].length
+        );
     }
 
-    private createContent(): string {
-        const rawContent = this.stringLines[this.currentLine];
+    private get width(): number {
+        return this.screen.info.cols - HORIZONTAL_MARGIN * 2;
+    }
+
+    private formatLine(rawContent: string, index: number): string {
+        if (index < this.currentLine) {
+            return `{green-bg}${rawContent}{/green-bg}`;
+        }
+        if (index > this.currentLine) {
+            return rawContent;
+        }
         const done = `{green-bg}${rawContent.substring(
             0,
             this.currentCol
@@ -107,7 +153,9 @@ export default class Text {
     }
 
     private render(): void {
-        this.lines[this.currentLine].setContent(this.createContent());
+        this.stringLines.forEach((line, index) => {
+            this.lines[index].setContent(this.formatLine(line, index));
+        });
         const TITLE = '——[ TypeSSH ]';
         this.header.setContent(
             TITLE +
@@ -116,10 +164,28 @@ export default class Text {
                     '—'
                 )
         );
+        this.score.setContent(`WPM: ${this.wpm()}`);
+        if (this.completed) {
+            this.scoreBox.show();
+        } else {
+            this.scoreBox.hide();
+        }
         this.screen.render();
     }
 
+    private restart(): void {
+        this.currentLine = 0;
+        this.startTime = 0;
+        this.errorString = '';
+        this.currentCol = 0;
+        this.render();
+    }
+
     private listen(): void {
+        this.screen.screen.key(['C-r'], () => {
+            this.restart();
+        });
+
         this.render();
         this.screen.screen.on('keypress', (key, keyInfo) => {
             if (!this.started) this.startTime = Date.now();
